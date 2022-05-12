91f296d2d540427b54e51fea2f3132b9e8f155a1
Michael Wechner <michael.wechner@digiswiss.ag>
2022-05-12 14:08:49 +0200

    Add compilation and installation step to build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 set VCPKG_CMAKE="%1/scripts/buildsystems/vcpkg.cmake"
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_CMAKE%
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_CMAKE% -DCMAKE_INSTALL_PREFIX=x64-windows-release\
+cmake --build . --config=Release
+cmake --install .
